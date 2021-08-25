38781187f7c8d47515e1f25e03012a71e0f8467b
nielsencs <nielsencs@users.noreply.github.com>
2021-08-25 12:49:54 +0100

    WiP

--- combineVersions.py
+++ combineVersions.py
@@ -15,7 +15,254 @@
     #store resolution
 
 
-tPath = 'E:\\GitHub\\bookish-lamp\\'
-tFileWEBO = 'generatedSQL\\WEB(2011)bible'         # + '.sql' or + 'NS.sql' 'Original' WEB
-tFileWEBC = 'generatedSQL\\USFM_2021-04-01_bible1' # + '.sql' or + 'NS.sql' 'Current'  WEB
-tFileBSMC = 'database\\bibleVerses.sql'            # 'Current'  BSM
+# tPath = 'E:\\GitHub\\bookish-lamp\\'
+# tFileWEBO = 'generatedSQL\\WEB(2011)bible'         # + '.sql' or + 'NS.sql' 'Original' WEB
+# tFileWEBC = 'generatedSQL\\USFM_2021-04-01_bible1' # + '.sql' or + 'NS.sql' 'Current'  WEB
+# tFileBSMC = 'database\\bibleVerses.sql'            # 'Current'  BSM
+# -----------------------------------------------------------------------------------------------------------------------------
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+from bibleModule import *
+
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import messagebox
+
+oTkWindow = tk.Tk()
+oTkWindow.title('Combine Bible Versions')
+oTkWindow.geometry('1680x300')
+oTkWindow.config(bg='skyblue')
+
+tTkFile1 = tk.StringVar()
+tTkFile2 = tk.StringVar()
+tTkLine1 = tk.StringVar()
+tTkLine2 = tk.StringVar()
+tTkLine3 = tk.StringVar()
+bTkLine1 = tk.BooleanVar()
+bTkLine2 = tk.BooleanVar()
+
+bTkLine1.set(True)
+
+tPath1 = 'generatedSQL'
+
+fr1 = ''
+fr2 = ''
+fw = ''
+tBook = ''
+bStarted = False
+
+tLine1 = ''
+tLine2 = ''
+# tLine3 = ''
+# ==============================================================================
+def getFile1():
+# ==============================================================================
+    global tFile1
+    tFile1 = tk.filedialog.askopenfilename(initialdir = tPath1,
+                                           title = 'Select file',
+                                           filetypes = (('SeQueL files','*.sql'),('TeXT files','*.txt'),('all files','*.*')))
+    tTkFile1.set(os.path.basename(tFile1))
+# ==============================================================================
+def getFile2():
+# ==============================================================================
+    global tFile2
+    tFile2 = tk.filedialog.askopenfilename(initialdir = tPath1,
+                                           title = 'Select file',
+                                           filetypes = (('SeQueL files','*.sql'),('TeXT files','*.txt'),('all files','*.*')))
+    tTkFile2.set(os.path.basename(tFile2))
+# ==============================================================================
+def setupTopFrame():
+# ==============================================================================
+    oFrameTop = tk.Frame(oTkWindow)
+    oFrameTop.grid(row = 0, column = 0, padx = 5, pady = 5)
+
+    iRow = 0
+
+    oLabel0 = tk.Label(oFrameTop, text='Setup')
+    oLabel0.grid(column = 0, row=iRow, padx = 5, pady = 5)
+
+    iRow = iRow + 1
+
+    oLabel1 = tk.Label(oFrameTop, text='Files')
+    oLabel1.grid(column = 0, row=iRow, padx = 5, pady = 5)
+
+    oText1 = tk.Entry(oFrameTop, textvariable = tTkFile1, width = 40)
+    oText1.grid(column = 1, row=iRow, padx = 5, pady = 5)
+
+    oBut1 = tk.Button(oFrameTop, command=getFile1, text='browse')
+    oBut1.grid(column = 2, row=iRow, padx = 5, pady = 5)
+
+    oText2 = tk.Entry(oFrameTop, textvariable = tTkFile2, width = 40)
+    oText2.grid(column = 4, row=iRow, padx = 5, pady = 5)
+
+    oBut2 = tk.Button(oFrameTop, command=getFile2, text='browse')
+    oBut2.grid(column = 5, row=iRow, padx = 5, pady = 5)
+
+    oBut3 = tk.Button(oFrameTop, command=processStart, text='Go')
+    oBut3.grid(column = 7, row=iRow, padx = 5, pady = 5)
+# ==============================================================================
+def setupMidFrame():
+# ==============================================================================
+    oFrameMid = tk.Frame(oTkWindow)
+    oFrameMid.grid(row = 1, column = 0, padx = 5, pady = 5)
+
+    iRow = 0
+
+    oLabel2 = tk.Label(oFrameMid, text='Process', justify = 'left')
+    oLabel2.grid(column = 0, row=iRow, padx = 5, pady = 5)
+
+    iRow = iRow + 1
+
+    oLabel3 = tk.Label(oFrameMid, text='A', justify = 'right')
+    oLabel3.grid(column = 0, row=iRow, padx = 5, pady = 5)
+
+    oText3 = tk.Entry(oFrameMid, textvariable = tTkLine1, width = 210)
+    oText3.grid(column = 1, row=iRow, padx = 5, pady = 5)
+
+    oChkBttn3 = tk.Checkbutton(oFrameMid, command=copyLine1, variable = bTkLine1)
+    oChkBttn3.grid(column = 2, row=iRow, padx = 0, pady = 0)
+
+    iRow = iRow + 1
+
+    oLabel4 = tk.Label(oFrameMid, text='B', justify = 'right')
+    oLabel4.grid(column = 0, row=iRow, padx = 5, pady = 5)
+
+    oText4 = tk.Entry(oFrameMid, textvariable = tTkLine2, width = 210)
+    oText4.grid(column = 1, row=iRow, padx = 5, pady = 5)
+
+    oChkBttn4 = tk.Checkbutton(oFrameMid, command=copyLine2, variable = bTkLine2)
+    oChkBttn4.grid(column = 2, row=iRow, padx = 0, pady = 0)
+
+    iRow = iRow + 1
+
+    oLabel5 = tk.Label(oFrameMid, text='>', justify = 'right')
+    oLabel5.grid(column = 0, row=iRow, padx = 5, pady = 5)
+
+    oText5 = tk.Entry(oFrameMid, textvariable = tTkLine3, width = 210)
+    oText5.grid(column = 1, row=iRow, padx = 5, pady = 5)
+
+    oBut5 = tk.Button(oFrameMid, command=processWrite, text='Write to File')
+    oBut5.grid(column = 3, row=iRow, padx = 5, pady = 5)
+# ==============================================================================
+def copyLine1():
+# ==============================================================================
+    if bTkLine1.get():
+        tTkLine3.set(tTkLine1.get())
+        bTkLine2.set(False) # set the other one false
+# ==============================================================================
+def copyLine2():
+# ==============================================================================
+    if bTkLine2.get():
+        tTkLine3.set(tTkLine2.get())
+        bTkLine1.set(False) # set the other one false
+# ==============================================================================
+def processStart():
+# ==============================================================================
+    global fr1
+    global fr2
+    global fw
+    global tBook
+    global bStarted
+    if len(tTkFile1.get()) == 0 or len(tTkFile2.get()) == 0:
+        messagebox.showinfo('Alert', 'Please select files first')
+    else:
+        buffer = ''
+
+        tWriteName = writeFileName(tFile1, tFile2, 'database')
+        fw = open(tWriteName, 'w', encoding="utf8")
+
+        fr1 = open(tFile1, 'r', encoding="utf8")
+        tLine1 = fr1.readline()
+        while not tLine1[:12] == 'INSERT INTO ':
+            fw.write(tLine1)
+            tLine1 = fr1.readline()
+        tBook = tLine1[82:85]
+        print('\n' + tBook)
+
+        fr2 = open(tFile2, 'r', encoding="utf8")
+        tLine2 = fr2.readline()
+        while not tLine2[:12] == 'INSERT INTO ':
+            tLine2 = fr2.readline()
+        processSame(tLine1, tLine2)
+        bStarted = True
+# ==============================================================================
+def processSame(tLine1, tLine2):
+# ==============================================================================
+    global fr1
+    global fr2
+    global fw
+    global tBook
+
+    while tLine1 == tLine2:
+        print('.', end='')
+        fw.write(tLine1)
+        tBookNew = tLine1[82:85]
+        if tBookNew != tBook:
+            tBook = tBookNew
+            print('\n' + tBook)
+        tLine1 = fr1.readline()
+        tLine2 = fr2.readline()
+
+    tTkLine1.set(tLine1[80:])
+    tTkLine2.set(tLine2[80:])
+    copyLine()
+# ==============================================================================
+def copyLine():
+# ==============================================================================
+    if bTkLine1.get():
+        tTkLine3.set(tTkLine1.get())
+    if bTkLine2.get():
+        tTkLine3.set(tTkLine2.get())
+# ==============================================================================
+def processWrite():
+# ==============================================================================
+    global fr1
+    global fr2
+    global fw
+    global tBook
+
+    print('D', end='')
+    fw.write('INSERT INTO `verses` (`bookCode`, `chapter`, `verseNumber`, `verseText`) VALUES ' + tTkLine3.get())
+
+    tLine1 = fr1.readline()
+    tLine2 = fr2.readline()
+    processSame(tLine1, tLine2)
+# ==============================================================================
+def processStop():
+# ==============================================================================
+    global fr1
+    global fr2
+    global fw
+    global bStarted
+
+    if bStarted:
+        fr1.close()
+        fr2.close()
+        fw.close()
+# ==============================================================================
+def writeFileName(tFile1, tFile2, tPath):
+# ==============================================================================
+    bDoIt = True
+    i = 0
+    tI = ''
+    while bDoIt:
+        tWriteName = ''
+        tWriteName = tWriteName + os.path.basename(tFile1)[:-4]
+        tWriteName = tWriteName + '-C-'
+        tWriteName = tWriteName + os.path.basename(tFile2)[:-4]
+        tWriteName = tWriteName + tI
+        tWriteName = tWriteName + '.sql'
+        if os.path.isfile(tPath + '\\' + tWriteName):
+            i = i + 1
+            tI = '(' + str(i) + ')'
+        else:
+            bDoIt = False
+    return tPath + '\\' + tWriteName
+
+setupTopFrame()
+setupMidFrame()
+
+oTkWindow.mainloop()
+
+processStop()
