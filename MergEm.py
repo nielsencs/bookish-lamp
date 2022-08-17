5d7d693e490a287e17ff6b6c0b1747e202c920f5
nielsencs <nielsencs@users.noreply.github.com>
2022-08-17 14:48:46 +0100

    Tidying up

--- MergEm.py
+++ MergEm.py
@@ -34,18 +34,20 @@ bTkLine3 = tk.BooleanVar()
 
 bTkLine1.set(True)
 
-tPath1 = 'generatedSQL'
-tPath2 = 'database'
+tPath1 = 'generatedSQL//'
+tPath2 = 'database//'
 
-tFileReadWNow = 'USFM_2021-08-18_bible'
+tExt____JustSeQuEl = '.sql'
 tExtGuessedStrongs = '0.sql'
 tExtUSFM___Strongs = '1.sql'
 tExtPlainNoStrongs = 'NS.sql'
 
 tFileReadOrig = 'WEB(2011)bible'
+tFileReadMine = 'bibleVerses'
+tFileReadWNow = 'USFM_2021-08-18_bible'
 
 tTkFileOrig.set(tFileReadOrig + tExtPlainNoStrongs)
-tTkFileMine.set('bibleVerses.sql')
+tTkFileMine.set(tFileReadMine + tExt____JustSeQuEl)
 tTkFileWNow.set(tFileReadWNow + tExtPlainNoStrongs)
 
 fw = ''
@@ -54,7 +56,7 @@ bStarted = False
 
 tLine1 = ''
 tLine2 = ''
-# tLine3 = ''
+tLine3 = ''
 # ==============================================================================
 def main():
 # ==============================================================================
@@ -141,7 +143,7 @@ def setupMidFrame(tTkLine1, bTkLine1, tTkLine2, bTkLine2, tTkLine3, bTkLine3, tT
     oText5 = tk.Entry(oFrameMid, textvariable = tTkLine3, width = 210)
     oText5.grid(column = 1, row=iRow, padx = 5, pady = 5)
 
-    oChkBttn5 = tk.Checkbutton(oFrameMid, command=copyLine2, variable = bTkLine3)
+    oChkBttn5 = tk.Checkbutton(oFrameMid, command=copyLine3, variable = bTkLine3)
     oChkBttn5.grid(column = 2, row=iRow, padx = 0, pady = 0)
 
     iRow = iRow + 1
@@ -157,133 +159,93 @@ def setupMidFrame(tTkLine1, bTkLine1, tTkLine2, bTkLine2, tTkLine3, bTkLine3, tT
 # ==============================================================================
 def processStart():
 # ==============================================================================
-    # global tTkFileOrig
+    global tFileReadOrig
+    global tFileReadMine
     global tFileReadWNow
-    global fr2
     global fw
     global tBook
     global bStarted
-
-    if len(tTkFileOrig.get()) == 0 or len(tTkFileMine.get()) == 0:
+############################################ FIX THIS! ############################################
+    if False: #len(tTkFileOrig.get()) == 0 or len(tTkFileMine.get()) or len(tTkFileWNow.get()) == 0:
         messagebox.showinfo('Alert', 'Please select files first')
     else:
+############################################ FIX THIS! ############################################
         buffer = ''
 
-        # tWriteName = writeFileName(tFile1, tFile2, 'database')
-        tWriteName = 'database\\combined.sql'
-        fw = open(tWriteName, 'w', encoding="utf8")
+        # tWriteName = 'database\\combined.sql'
+        # fw = open(tWriteName, 'w', encoding="utf8")
 
-        tFileReadWNow = open(tFile1, 'r', encoding="utf8")
-        tLine1 = tFileReadWNow.readline()
+        tFileReadOrig = open(tPath1 + tTkFileOrig.get(), 'r', encoding="utf8")
+        tFileReadMine = open(tPath2 + tTkFileMine.get(), 'r', encoding="utf8")
+        tFileReadWNow = open(tPath1 + tTkFileWNow.get(), 'r', encoding="utf8")
+
+        tLine1 = tFileReadOrig.readline()
         while not tLine1[:12] == 'INSERT INTO ':
-            fw.write(tLine1)
-            tLine1 = tFileReadWNow.readline()
+            # fw.write(tLine1)
+            tLine1 = tFileReadOrig.readline()
         tBook = tLine1[82:85]
         print('\n' + tBook)
 
-        fr2 = open(tFile2, 'r', encoding="utf8")
-        tLine2 = fr2.readline()
+        tLine2 = tFileReadMine.readline()
         while not tLine2[:12] == 'INSERT INTO ':
-            tLine2 = fr2.readline()
-        processSame(tLine1, tLine2)
+            # fw.write(tLine2)
+            tLine2 = tFileReadMine.readline()
+
+        tLine3 = tFileReadWNow.readline()
+        while not tLine3[:12] == 'INSERT INTO ':
+            # fw.write(tLine3)
+            tLine3 = tFileReadWNow.readline()
+
+        processSame(tLine1, tLine2, tLine3)
         bStarted = True
 # ==============================================================================
-def processSame(tLine1, tLine2):
+def processSame(tLine1, tLine2, tLine3):
 # ==============================================================================
+    global tFileReadOrig
+    global tFileReadMine
     global tFileReadWNow
-    global fr2
     global fw
     global tBook
 
     while tLine1 == tLine2:
-        print('.', end='')
-        fw.write(tLine1)
+        # print('.', end='')
+        # fw.write(tLine1)
         tBookNew = tLine1[82:85]
         if tBookNew != tBook:
             tBook = tBookNew
             print('\n' + tBook)
-        tLine1 = tFileReadWNow.readline()
-        tLine2 = fr2.readline()
+        tLine1 = tFileReadOrig.readline()
+        tLine2 = tFileReadMine.readline()
+        tLine3 = tFileReadWNow.readline()
 
     tTkLine1.set(tLine1[80:])
     tTkLine2.set(tLine2[80:])
+    tTkLine3.set(tLine3[80:])
+
     copyLine()
 # ==============================================================================
 def copyLine():
 # ==============================================================================
     if bTkLine1.get():
-        tTkLine3.set(tTkLine1.get())
+        tTkLine4.set(tTkLine1.get())
     if bTkLine2.get():
-        tTkLine3.set(tTkLine2.get())
+        tTkLine4.set(tTkLine2.get())
+    if bTkLine3.get():
+        tTkLine4.set(tTkLine3.get())
 # ==============================================================================
 def processStop():
 # ==============================================================================
+    global tFileReadOrig
+    global tFileReadMine
     global tFileReadWNow
-    global fr2
     global fw
     global bStarted
 
     if bStarted:
+        tFileReadOrig.close()
+        tFileReadMine.close()
         tFileReadWNow.close()
-        fr2.close()
-        fw.close()
-# ==============================================================================
-def startSession():
-# ==============================================================================
-    global iScriptLine
-    global stream
-    global p
-    global frames
-    global MMdata
-
-
-    p = pyaudio.PyAudio()
-    stream = None
-    stream = p.open(format = FORMAT,
-                    channels = CHANNELS,
-                    rate = RATE,
-                    input = True,
-                    frames_per_buffer = CHUNK)
-    frames = []
-    MMdata = np.zeros((0,3))
-    print("* starting recording session for line " + str(iScriptLine + start_line))
-# ==============================================================================
-def finishSession():
-# ==============================================================================
-    global iScriptLine
-    global stream
-    global p
-    global frames
-    global tWavFiles
-
-    bStart = True
-
-    print("* done recording session for line " + str(iScriptLine + start_line))
-
-    stream.stop_stream()
-    stream.close()
-    p.terminate()
-
-    frames_with_content = []
-
-    iLen = len(frames)
-    # for i in range(EDGE_MARGIN, iLen-EDGE_MARGIN):
-    for i in range(EDGE_MARGIN, iLen):
-        keepMarginWindowStart = max(0, i - KEEP_MARGIN)
-        keepMarginWindowEnd = min(iLen, i + KEEP_MARGIN+1)
-        if bKeepPauses and not bStart:
-            frames_with_content.append(frames[i])
-        elif np.sum(MMdata[keepMarginWindowStart:keepMarginWindowEnd, 2]) >= 1: # if this chunk, or any of its neighbouring chunks, "has content", include it.
-            frames_with_content.append(frames[i])
-            bStart = False
-
-    # tWavFile = tPath + '/output' + str(iScriptLine) + '.wav' # restore this if you want to go back to just numbered wave files
-    tWavFile = tBook + '_' + str(iChapter).zfill(3) + '_' + str(iScriptLine).zfill(3) + '.wav' # chapter and verse named wave files
-    tWavFiles = buildWavFileList(tWavFiles, tWavFile, iScriptLine)
-    # tWavFiles[iScriptLine] = tWavFile
-    writeWaveFile(tPath + '/' + tWavFile, frames_with_content, p.get_sample_size(FORMAT), CHANNELS, FORMAT, RATE)
-
-    stream = None
+        # fw.close()
 # ==============================================================================
 def getFile1():
 # ==============================================================================
@@ -303,37 +265,49 @@ def getFile2():
 # ==============================================================================
 def getFile3():
 # ==============================================================================
-    global tFile2
-    tFile2 = tk.filedialog.askopenfilename(initialdir = tPath1,
+    global tFile3
+    tFile3 = tk.filedialog.askopenfilename(initialdir = tPath1,
                                            title = 'Select file',
                                            filetypes = (('SeQueL files','*.sql'),('TeXT files','*.txt'),('all files','*.*')))
-    tTkFileMine.set(os.path.basename(tFile2))
+    tTkFileMine.set(os.path.basename(tFile3))
 # ==============================================================================
 def copyLine1():
 # ==============================================================================
     if bTkLine1.get():
-        tTkLine3.set(tTkLine1.get())
-        bTkLine2.set(False) # set the other one false
+        tTkLine4.set(tTkLine1.get())
+        bTkLine2.set(False) # set the other two false
+        bTkLine3.set(False) # set the other two false
 # ==============================================================================
 def copyLine2():
 # ==============================================================================
     if bTkLine2.get():
-        tTkLine3.set(tTkLine2.get())
-        bTkLine1.set(False) # set the other one false
+        tTkLine4.set(tTkLine2.get())
+        bTkLine1.set(False) # set the other two false
+        bTkLine3.set(False) # set the other two false
+# ==============================================================================
+def copyLine3():
+# ==============================================================================
+    if bTkLine3.get():
+        tTkLine4.set(tTkLine3.get())
+        bTkLine1.set(False) # set the other two false
+        bTkLine2.set(False) # set the other two false
 # ==============================================================================
 def processWrite():
 # ==============================================================================
+    global tFileReadOrig
+    global tFileReadMine
     global tFileReadWNow
-    global fr2
     global fw
     global tBook
 
     print('D', end='')
-    fw.write('INSERT INTO `verses` (`bookCode`, `chapter`, `verseNumber`, `verseText`) VALUES ' + tTkLine3.get())
+    # fw.write('INSERT INTO `verses` (`bookCode`, `chapter`, `verseNumber`, `verseText`) VALUES ' + tTkLine3.get())
 
-    tLine1 = tFileReadWNow.readline()
-    tLine2 = fr2.readline()
-    processSame(tLine1, tLine2)
+    tLine1 = tFileReadOrig.readline()
+    tLine2 = tFileReadMine.readline()
+    tLine3 = tFileReadWNow.readline()
+
+    processSame(tLine1, tLine2, tLine3)
 # ------------------------------------------------------------------------------
 main()
 # ------------------------------------------------------------------------------
