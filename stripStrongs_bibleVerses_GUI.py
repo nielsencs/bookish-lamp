8baa3922bbea390acb64b98aa73a2e5c070bfce2
Carl Nielsen <nielsencs@users.noreply.github.com>
2024-05-29 15:30:26 +0100

    Create stripStrongs_bibleVerses_GUI.py

---
+++ stripStrongs_bibleVerses_GUI.py
@@ -0,0 +1,47 @@
+from bibleModule import writeFileName, stripStrongs
+import PySimpleGUI as sg
+#globals
+tPath1 = 'generatedSQL'
+
+def writeBibleVerses():
+    tFile1 = 'D:/Python/bookish-lamp/database/bibleVerses.sql'
+
+    tWriteName = writeFileName(tFile1)
+    fw = open(tPath1 + '\\' + tWriteName, 'w', encoding="utf8")
+
+    fr1 = open(tFile1, 'r', encoding="utf8")
+    tLine1 = fr1.readline()
+
+    while tLine1:
+        tLine1, tDot = stripStrongs(tLine1)
+        print(tDot, end='')
+        fw.write(tLine1)
+        tLine1 = fr1.readline()
+
+    fr1.close()
+    fw.close()
+
+def gui():
+    sg.theme('Dark Blue 3')
+    app_name = 'stripStrongs_bibleVerses_GUI.py'
+
+    layout = [
+                [sg.Text(app_name, font=("Helvetica", 25))],
+                [sg.Button('Go', key='bGO')],
+            ]
+
+    window = sg.Window(app_name, layout)
+    b_do_it = True
+    while b_do_it:
+        event, _ = window.read()
+        if event:
+            if event == 'bGO':
+                writeBibleVerses()
+        else:
+            b_do_it = False
+
+
+def get_name (text):
+    return text.replace(" ", "").replace(":", "_").strip()
+
+gui()
